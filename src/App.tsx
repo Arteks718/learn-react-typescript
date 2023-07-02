c9dc073c487fac22120d8a786420df95e13402b7
Arteks718 <artexbro.official@gmail.com>
2023-07-02 16:39:23 +0300

    Button counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import React, {createElement as e} from 'react';
+import React, {createElement as e, useState} from 'react';
 
 function App() {
-  // return (
-  //   <h1>Hello React!!!</h1>
-  // );
+  const [count, setCount] = useState(0)
   return e('div', {className: 'container'}, 
     [
-      e('h1', {classname: 'font-bold'}, 'Test JSX'), 
-      e('button', {classname: ''}, 'Click on me')
+      e('h1', {className: 'font-bold', key: 1}, 'Test JSX'), 
+      e('button', {className: 'py-2 px-4 border', key: 2, onClick: () => setCount(count+1) }, `Click on me ${count}`)
     ])
 }
 
